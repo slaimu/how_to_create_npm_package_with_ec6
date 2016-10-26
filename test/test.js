5f20db98b0d6952f3b1cc3d3ff2ab701b260a8e5
Xiaowu Zhang <xiaowu.zhangv@gmail.com>
2016-10-27 01:44:24 +0800

    add test

---
+++ test/test.js
@@ -0,0 +1,33 @@
+import assert from 'assert';
+import path from 'path';
+import fs from 'fs';
+import download from '../src';
+import {randomFilename} from '../src/utils';
+
+let readFile = f => fs.readFileSync(f).toString();
+let getFileSize = f => fs.statSync(f).size;
+
+describe('test download', () => {
+
+  it('copy local file', done => {
+
+    let source = __filename;
+    let target = randomFilename();
+    let onProgress = false;
+    console.log(source, target);
+    download(source, target, (size, total) => {
+      onProgress = true;
+      assert.equal(size, total);
+      assert.equal(total, getFileSize(source));
+      
+    }).then(filename => {
+      assert.equal(onProgress, true);
+      assert.equal(target, filename);
+      assert.equal(readFile(source), readFile(target));
+      done();
+    }).catch(err => {
+      throw err;
+    });
+  });
+
+});
